be77883e458bea969d74406d2283f9acae61e33b
ANtonio García <jose.ramirez@cimat.mx>
2020-12-18 00:12:52 -0600

    se restaura original con ngrok

--- DJ4E_Specialization/WebAppTech&Django/django_projects/mysite/mysite/urls.py
+++ DJ4E_Specialization/WebAppTech&Django/django_projects/mysite/mysite/urls.py
@@ -16,11 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include 
 
-from . import views #nomas para que jale en mi local 
-
 
 urlpatterns = [
-    path('', views.index_m, name='index_m'), #nomas para que jale en mi local 
     path('polls/', include('polls.urls')), 
     path('admin/', admin.site.urls),
 ]
