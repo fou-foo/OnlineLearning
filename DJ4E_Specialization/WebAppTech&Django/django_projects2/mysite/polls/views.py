f5f417b493e9cd4f518f1be8afdfce129f21f204
ANtonio García <jose.ramirez@cimat.mx>
2020-12-19 20:31:47 -0600

    se acaba curso uno, se empieza el dos

--- DJ4E_Specialization/WebAppTech&Django/django_projects2/mysite/polls/views.py
+++
@@ -1,8 +0,0 @@
-# no se que haga asi venia -- from django.shortcuts import render
-from django.http import HttpResponse
-
-# Create your views here.
-def index(request):
-    	return( HttpResponse("Hello, world. b63c87b0 is the polls index.") )
-
-    	
