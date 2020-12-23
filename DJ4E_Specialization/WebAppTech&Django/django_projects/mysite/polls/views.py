c860d503385496ea9ef8c74ae5d1967380096e98
ANtonio García <jose.ramirez@cimat.mx>
2020-12-22 23:25:43 -0600

    se sube test de semana 2

--- DJ4E_Specialization/WebAppTech&Django/django_projects/mysite/polls/views.py
+++ DJ4E_Specialization/WebAppTech&Django/django_projects/mysite/polls/views.py
@@ -1,8 +1,33 @@
 # no se que haga asi venia -- from django.shortcuts import render
 from django.http import HttpResponse
+#from django.http import Http404
+
+#from django.template import loader
+from django.shortcuts import get_object_or_404, render
+
+
+from .models import Question
 
 # Create your views here.
 def index(request):
-    	return( HttpResponse("Hello, world. b63c87b0 is the polls index.") )
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
+	#return( HttpResponse("Hello, world. b63c87b0 is the polls index.") )
 
-    	
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
+
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
+
+def owner(request):
+       return HttpResponse("Hello, world. 38a8e18d is the polls index.")
+
